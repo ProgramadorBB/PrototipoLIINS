704df7dd4ddc1ef906b8d79c39f5a0f88fcf60bf
BYRON MATIAS BRAVO GARCIA <byron.bravo@inacapmail.cl>
2019-06-23 06:37:03 -0400

    Se agrego el boton "Volver" evitando el error que al presionar 2 veces intercale y pase largo al hacer un "back" en la navegación de la aplicación

--- PrototipoLIINS/PrototipoLIINS/Vistas/VistaLegal.xaml.cs
+++ PrototipoLIINS/PrototipoLIINS/Vistas/VistaLegal.xaml.cs
@@ -15,6 +15,21 @@ namespace PrototipoLIINS.Vistas
 		public VistaLegal ()
 		{
 			InitializeComponent ();
-		}
-	}
+            NavigationPage.SetHasNavigationBar(this, false);
+            int cont = 0;
+            Application.Current.Properties["contador"] = cont;
+        }
+
+        private async void BtnVolver_Clicked(object sender, EventArgs e)
+        {
+            int cont = (Int32)Application.Current.Properties["contador"];
+
+            if (cont < 1)
+            {
+                cont = cont + 1;
+                Application.Current.Properties["contador"] = cont;
+                await Navigation.PopAsync();
+            }
+        }
+    }
 }
